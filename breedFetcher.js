fe1189794e83bc30c81bac694b1e57e7878c4e80
vagrant <wong_michael@rogers.com>
2021-11-20 23:01:51 +0000

    Finish breedFetcher.js

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,1 +1,17 @@
-const request = require('request');
+const request = require("request");
+
+request(
+  `https://api.thecatapi.com/v1/breeds/search?q=${process.argv[2]}`,
+  function(error, response, body) {
+    if (error) {
+      console.log(error);
+      return;
+    }
+    const data = JSON.parse(body);
+    if (data[0]) {
+      console.log(data[0].description);
+    } else {
+      console.log("Does not exist");
+    }
+  }
+);
